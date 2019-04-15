a7a123e646a5f61978871241003b6c55ada7d68e
junshi2019 <49259452+junshi2019@users.noreply.github.com>
2019-04-15 23:47:58 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,6 @@ namespace app\index\controller;
 class Index extends \think\Controller
 {
     public function index(){ 
-       return 'This app run success...';
+       return '你好，程序运行正常。你可进一步开发.';
     }
 }
