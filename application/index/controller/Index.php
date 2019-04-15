3303a6f9395d05d1f81bab60b6ed78551b2aec20
junshi2019 <49259452+junshi2019@users.noreply.github.com>
2019-04-15 23:52:35 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,6 @@ namespace app\index\controller;
 class Index extends \think\Controller
 {
     public function index(){ 
-       return '你好，程序运行正常。你可进一步开发.<a href="phpinfo.php">phpinfo</a>';
+       return '<div align="center">你好,程序运行正常.你可进一步开发.&nbsp;&nbsp;<a href="phpinfo.php">phpinfo</a></div>';
     }
 }
