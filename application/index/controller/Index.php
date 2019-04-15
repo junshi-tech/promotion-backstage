744f122ad3d16abf4b139d31309151e0277641aa
junshi2019 <49259452+junshi2019@users.noreply.github.com>
2019-04-16 00:14:52 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,9 +1,17 @@
 <?PHP
 namespace app\index\controller;
 
+use think\Db;
+
 class Index extends \think\Controller
 {
     public function index(){ 
        return '<div align="center" style="margin-top: 20px; background-color: yellow;">你好,程序运行正常.你可进一步开发.&nbsp;&nbsp;<a href="phpinfo.php">phpinfo</a></div>';
+    }
+    
+    public function junshi(){ 
+        $list = Db::table('user')->select();
+        print_r($list);
+        die();
     }
 }
